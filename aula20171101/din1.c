4507be9e1dd0af1bc09093e4453cb601b71f2f29
Alex <deneralex39@gmail.com>
2017-11-01 11:45:25 -0200

    fim de aula

---
+++ aula20171101/din1.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<math.h>
+float media(float * numeros, int qtde)
+{
+float m= 0.0f;
+int i;
+for(i=0; i<qtde; i++)
+m+=numeros[i];
+m/=qtde;
+return m;
+}
+
+float desviopadrao(float * numeros, int qtde){
+float m= media(numeros, qtde);
+float dp= 0.0f;
+int i;
+for(i=0; i<qtde; i++)
+dp+= pow(numeros[i]-m,2);
+dp/=qtde-1;
+return sqrt(dp);
+}
+
+
+int main(){
+int qtde, i=0;
+float *numeros;
+printf("Quantos numeros voce precisa");
+scanf("%d", &qtde);
+numeros= (float*)malloc(qtde*sizeof(float));// a função malloc retorna um endereço do bloco de memoria que foi alocado, por um ponteiro, no caso do tipo float.
+for(i=0; i<qtde; i++)
+{
+printf("Entre com o %d o numero", i+1);
+scanf("%f", numeros+i);
+}
+printf("A media dos numeros e: %f\n", media(numeros, qtde));
+printf("O desvio padrao dos numeros e: %f\n", desviopadrao(numeros,qtde));
+free(numeros);
+return 0;
+
+
+
+
+}
