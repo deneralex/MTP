56debdea9249a5cac24ec0224d5d95aca69481f7
Alex <deneralex39@gmail.com>
2017-11-26 00:51:58 -0200

    fim de aula

--- aula20171101/din3.c
+++ aula20171101/din3.c
@@ -1,22 +1,33 @@
 #include<stdlib.h>
 #include<stdio.h>
-#include<math.h>
+#include <ctype.h>
 int main()
 {
-    char * rec;
+    char * str; //declara um ponteiro para char
     int i=0;
-    rec=(char*)malloc(1*sizeof(char));
-    printf("write one phrase:\n");
-    while(1)
+    str=(char*)calloc(1,sizeof(int));//o ponteiro para char recebe um endereço na memoria reservado dinamicamente pelo calloc
+    printf("Entre com uma frase:\n");
+    while(!0)//laço infinito
+    {
+        scanf("%c", str+i);//
+        str=(char*)realloc(str, sizeof(char)*1);
+        if((str+i)==NULL)//se o valor retornado para o ponteiro for NULL
         {
-            scanf("%c", rec+i);
-            rec= (char *)realloc(rec, sizeof(char)*1);
-            if(*(rec+i)==' ')
-                break;
-            i++;
+            printf("\n\nSem memoria suficiente!\n\n");
+            return EXIT_SUCCESS;
         }
-        *(rec+i)='\0';
-        printf("%s",rec);
-        free(rec);
-    return 0;
+        if(*(str+i)=='*')
+            break;
+        i++;
+    }
+    *(str+i)='\0';
+    while(i>=0)
+    {
+        *(str+i)= toupper(*(str+i));
+        i--;
+    }
+
+    printf("Frase salva na memoria (maiuscula): %s \n\n",str);
+    free(str);
+    return EXIT_SUCCESS;
 }
